9ff435d6f51e653af5e49f33640c701fef128a71
laeti <laetitia_sfeir@hotmail.com>
2024-02-08 13:08:56 +0100

    initial commit

--- main.py
+++ main.py
@@ -2,39 +2,39 @@ from datetime import datetime
 
 import pandas as pd
 
-import Repository
-import View
+import repository
+import view
 
-path = r"C:\Users\laeti\OneDrive\Desktop\Master 2 BF\Autre\projet maquette\account.invoice (1).xlsx"
+path = config["input"]["input_path"]
 
 # get data from excel_file
-df_bq = pd.read_excel(path, sheet_name="bq")
-df_erp = pd.read_excel(path, sheet_name="ERP")
-excel = pd.read_excel(path, sheet_name="tableau")
+df_bq = pd.read_excel(path, sheet_name = config["input"]["sheet_bank"])
+df_erp = pd.read_excel(path, sheet_name=config["input"]["sheet_server"])
+excel = pd.read_excel(path, sheet_name=config["input"]["sheet_data"])
 excel['Date'] = excel['Date'].apply(lambda x: datetime.strftime(x, "%B"))
 
 column_status = 'Status'
 column_amount = 'Montant1'
 
 # format and merge dataframes
-df_bq = Repository.reformat_file(df_bq)
-df_erp = Repository.reformat_file(df_erp)
+df_bq = repository.reformat_file(df_bq)
+df_erp = repository.reformat_file(df_erp)
 inner_join = pd.merge(df_erp, df_bq, on='Numéro', how='inner')
-Repository.get_status(df=inner_join, column_status=column_status)
+repository.get_status(df=inner_join, column_status=column_status)
 
 # Open excel workbook and print the status in sheet BQ
-View.print_data(path=path, df=inner_join, status=column_status)
+view.print_data(path=path, df=inner_join, status=column_status)
 
 # get invoices and amounts for which a reminder e-mail must be sent
-final_list = Repository.get_list_reminder(df_erp)
+final_list = repository.get_list_reminder(df_erp)
 
 # sent reminder email
-View.mail("oceane.guiovanna@gmail.com", "lvlk xsbt zjkt dfjh", "Laetitia_sfeir@hotmail.com", "Mail de relance",
+view.mail("oceane.guiovanna@gmail.com", "lvlk xsbt zjkt dfjh", "Laetitia_sfeir@hotmail.com", "Mail de relance",
           final_list)
 
 # creating the necessary dataframe for graphic and table
-table1 = Repository.create_tables(excel, column_amount, column_status)[0]
-table2 = Repository.create_tables(excel, column_amount, column_status)[1]
+table1 = repository.create_tables(excel, column_amount, column_status)[0]
+table2 = repository.create_tables(excel, column_amount, column_status)[1]
 
 # generate chart and pdf
-View.graph(table1, table2)
+view.graph(table1, table2)
